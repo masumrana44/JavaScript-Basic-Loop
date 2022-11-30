f7529a40f9448fe550bc80372971b1c7b3276e5a
Masum-Rana <masumrana446267@gmail.com>
2022-11-30 12:55:55 +0600

    JavaScript-Loop

---
+++ loop.js
@@ -0,0 +1,100 @@
+// 1.What is javaScript ?
+// 2.What is Variable ?
+// 3.How to declare a varibale ?
+// 4.what is  JavaScript DataType?
+// 5.What is array ?
+// 6.How can i declare an array?
+// 7.how can we make a condition in js ?
+// 8.How can we use multi Condition in javaScript?
+// 9.JavaScript shorthand in dettails.
+
+
+// what is loop in javaScript ?
+// amr loop ta jotokhon amr loop er condition true hobe totokhon poinzonto loop colte thakbe.r false hole loop run kora bondo hoye jabe
+
+// var index = 1;
+// while (index<=10) {
+//     console.log('Shit asche biye korte cay');
+//     // index = index + 1;
+//     // index += 1;
+//     index++;
+
+// };
+
+
+// Get all element an array with for loop
+// var phones = ['Nokia-110', 'Nokiac-25', 'Maximus', 'lava', 'Samsung Grand Prime', 'Samsung Galaxy -5', 'Vivo -y50 '];
+
+
+// for (i = 0; i < phones.length;i++){
+//     var element = phones[i];
+//     console.log(element)
+
+// }
+
+
+
+
+
+// for (var i = 1; i <= 10; i++){
+//     console.log('Ami biye korte cay');
+// }
+
+
+
+// var number = 0;
+// while (number<50) {
+//     if (number % 2 === 0) {
+//         console.log(number)
+//     }
+//     number++
+// }
+
+
+// for (i = 0; i < 100; i++){
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+// for (i = 0; i < 100; i++){
+//     if (i % 2 != 0) {
+//         console.log(i)
+//     }
+// }
+
+
+
+
+
+
+/*
+Problems solving
+====================
+
+1.Display "ajke amr mon bhalo nei" in 30 times;
+2.Display numbers between 58 to 98;
+3.Show all even numbers from 240 to 380;
+5.show all odd numbers between 820 to 100;
+6.What's differnce between while loop and for loop?
+7.Declare an array for all the elements of the by using a while loop;
+8.Declare an array for all the elements of the by using a for loop;
+9.Create an array for all the mobiles phones.Display all the elements of the using for loop?
+10.Create an array and write price fo the books that you have.Display the prices if the prices is lower 250;
+
+ /*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
